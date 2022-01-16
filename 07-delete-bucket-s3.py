d2ab7bce2c5632434890c4dc029aaf2a438e0f01
Oscar Valles <oscar@findyourpod.today>
2022-01-15 23:56:18 -0600

    S3 python course

---
+++ 07-delete-bucket-s3.py
@@ -0,0 +1,24 @@
+import os
+import boto3
+
+# instantiate S3 connection
+client = boto3.client('s3')
+
+# path, bucket and file vars
+target_bucket = 's3-osk-empty-bucket'
+
+# retrieve bucket list
+response = client.list_buckets()
+
+for n in response['Buckets']:
+    print(n['Name'])
+
+client.delete_bucket(
+    Bucket=target_bucket
+)
+
+# refresh bucket list and print out
+response = client.list_buckets()
+
+for n in response['Buckets']:
+    print(n['Name'])
